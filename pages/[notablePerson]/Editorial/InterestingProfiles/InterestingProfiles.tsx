39fec0f5324a8482e2f320057dd066a5bc9458f1
M.K. Safi <msafi@msafi.com>
2021-12-22 14:08:44 -0500

    no message

--- pages/[notablePerson]/Editorial/InterestingProfiles/InterestingProfiles.tsx
+++ pages/[notablePerson]/Editorial/InterestingProfiles/InterestingProfiles.tsx
@@ -7,29 +7,33 @@ import Image from 'next/image';
 export const InterestingProfiles = (p: {
   interestingProfiles: TEditorial['data']['interestingProfiles'];
 }) => {
-  return p.interestingProfiles.map((notablePersonData) => {
-    return (
-      <a
-        className={s.interestingProfile}
-        href={`/${notablePersonData.id}`}
-        key={notablePersonData.id}
-      >
-        <Image
-          src={`/images/notablePeople/${notablePersonData.id}.jpg`}
-          alt={notablePersonData.name}
-          width={120}
-          height={120}
-          className={s.interestingProfileImage}
-        />
-        <Typography
-          fontWeight={500}
-          variant="h4"
-          component="p"
-          className={s.interestingProfileName}
-        >
-          {notablePersonData.name}
-        </Typography>
-      </a>
-    );
-  });
+  return (
+    <>
+      {p.interestingProfiles.map((notablePersonData) => {
+        return (
+          <a
+            className={s.interestingProfile}
+            href={`/${notablePersonData.id}`}
+            key={notablePersonData.id}
+          >
+            <Image
+              src={`/images/notablePeople/${notablePersonData.id}.jpg`}
+              alt={notablePersonData.name}
+              width={120}
+              height={120}
+              className={s.interestingProfileImage}
+            />
+            <Typography
+              fontWeight={500}
+              variant="h4"
+              component="p"
+              className={s.interestingProfileName}
+            >
+              {notablePersonData.name}
+            </Typography>
+          </a>
+        );
+      })}
+    </>
+  );
 };
