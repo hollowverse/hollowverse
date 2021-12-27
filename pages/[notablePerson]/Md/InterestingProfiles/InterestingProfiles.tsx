aa37e28f0a79ba1f9b028c04d9afc96ab443682b
M.K. Safi <msafi@msafi.com>
2021-12-27 17:43:12 -0500

    no message

--- pages/[notablePerson]/Md/InterestingProfiles/InterestingProfiles.tsx
+++ pages/[notablePerson]/Md/InterestingProfiles/InterestingProfiles.tsx
@@ -1,44 +1,55 @@
 import React from 'react';
-import s from '../../notablePerson.module.scss';
+import s from './InterestingProfiles.module.scss';
 import { TNotablePersonMd } from '_r/pages/common/types';
 import { Typography } from '@mui/material';
 import Image from 'next/image';
+import { Icon } from '_r/pages/common/Icon';
+import StarsIcon from '_i/icons/stars.svg';
 
 export const InterestingProfiles = (p: {
   relatedPeople: TNotablePersonMd['data']['relatedPeople'];
 }) => {
   return (
-    <>
-      {p.relatedPeople.map((notablePersonData) => {
-        return (
-          <a
-            className={s.interestingProfile}
-            href={`/${notablePersonData.slug}`}
-            key={notablePersonData.slug}
-          >
-            <span className={s.interestingProfileImageContainer}>
-              <Image
-                objectFit="cover"
-                objectPosition="top"
-                src={notablePersonData.pic || '/images/avatar-placeholder.png'}
-                alt={notablePersonData.name}
-                layout="fixed"
-                width={160}
-                height={200}
-                className={s.interestingProfileImage}
-              />
-            </span>
-            <Typography
-              fontWeight={500}
-              variant="h4"
-              component="p"
-              className={s.interestingProfileName}
+    <div className={s.InterestingProfiles}>
+      <Typography variant="h1" component="h3">
+        <Icon component={StarsIcon} />
+        Other interesting profiles
+      </Typography>
+
+      <div className={s.content}>
+        {p.relatedPeople.map((notablePersonData) => {
+          return (
+            <a
+              className={s.link}
+              href={`/${notablePersonData.slug}`}
+              key={notablePersonData.slug}
             >
-              {notablePersonData.name}
-            </Typography>
-          </a>
-        );
-      })}
-    </>
+              <span className={s.image}>
+                <Image
+                  objectFit="cover"
+                  objectPosition="top"
+                  src={
+                    notablePersonData.pic || '/images/avatar-placeholder.png'
+                  }
+                  alt={notablePersonData.name}
+                  layout="fixed"
+                  width={160}
+                  height={200}
+                  className={s.image}
+                />
+              </span>
+              <Typography
+                fontWeight={500}
+                variant="h4"
+                component="p"
+                className={s.name}
+              >
+                {notablePersonData.name}
+              </Typography>
+            </a>
+          );
+        })}
+      </div>
+    </div>
   );
 };
--- pages/[notablePerson]/Md/Md.tsx
+++ pages/[notablePerson]/Md/Md.tsx
@@ -1,9 +1,6 @@
-import { Container, Typography } from '@mui/material';
+import { Container } from '@mui/material';
 import React, { useState } from 'react';
-import StarsIcon from '_i/icons/stars.svg';
-import { Icon } from '_r/pages/common/Icon';
 import { NotablePersonProps } from '_r/pages/[notablePerson].page';
-import s from '../notablePerson.module.scss';
 import { Article } from './Article/Article';
 import { InterestingProfiles } from './InterestingProfiles/InterestingProfiles';
 import { Sources } from './Sources/Sources';
@@ -16,11 +13,7 @@ export const Md = (p: NotablePersonProps) => {
   }
 
   return (
-    <Container
-      maxWidth="md"
-      className={s.editorialContainer}
-      component="section"
-    >
+    <Container maxWidth="md" component="section" style={{ marginTop: 32 }}>
       <Article
         notablePersonMd={p.notablePersonMd}
         setShowSources={setShowSources}
@@ -34,18 +27,9 @@ export const Md = (p: NotablePersonProps) => {
         />
       )}
 
-      <div className={s.interestingProfilesContainer}>
-        <Typography variant="h1" component="h3">
-          <Icon component={StarsIcon} />
-          Other interesting profiles
-        </Typography>
-
-        <div className={s.interestingProfiles}>
-          <InterestingProfiles
-            relatedPeople={p.notablePersonMd.data.relatedPeople}
-          />
-        </div>
-      </div>
+      <InterestingProfiles
+        relatedPeople={p.notablePersonMd.data.relatedPeople}
+      />
     </Container>
   );
 };
