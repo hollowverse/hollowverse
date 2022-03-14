d4bb17a31593217d4acc79d530ce003dce7605dc
M.K. Safi <msafi@msafi.com>
2022-03-14 10:21:18 -0700

    Move _app components

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -8,10 +8,10 @@ import 'normalize.css';
 import React from 'react';
 import { StaticPropsContextProvider } from '~/components/StaticPropsContextProvider';
 import { theme } from '~/pages/common/theme';
-import { Footer } from '~/pages/_app/Footer/Footer';
-import '~/pages/_app/globalStyles.css';
-import { Head } from '~/pages/_app/Head/Head';
-import { AppBar } from './_app/AppBar/AppBar';
+import { Footer } from '~/components/Footer/Footer';
+import '~/components/globalStyles.css';
+import { Head } from '~/components/Head/Head';
+import { AppBar } from '~/components/AppBar/AppBar';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
