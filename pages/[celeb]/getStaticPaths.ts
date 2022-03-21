067eee51376d37bdf9dc0101c92ebb38aae8c849
M.K. Safi <msafi@msafi.com>
2022-03-21 16:31:30 -0700

    Fix getStaticPaths

--- pages/[celeb]/getStaticPaths.ts
+++ pages/[celeb]/getStaticPaths.ts
@@ -7,6 +7,6 @@ export const getStaticPaths = async () => {
 
   return {
     paths,
-    fallback: true,
+    fallback: false,
   };
 };
--- pages/[celeb]/paths.ts
+++
@@ -1,5 +0,0 @@
-import path from 'path';
-import appRoot from 'app-root-path';
-
-export const celebsPath = path.join(appRoot.path, 'celebs');
-export const publicDir = path.join(appRoot.path, 'public');
