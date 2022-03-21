8718689ae128473b66703fc2af8156dbaf7d5ff5
M.K. Safi <msafi@msafi.com>
2022-03-21 13:18:25 -0700

    no message

--- pages/_middleware.page.ts
+++
@@ -1,9 +0,0 @@
-// import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
-
-// export function middleware(req: NextRequest, ev: NextFetchEvent) {
-//   const pathname = req.nextUrl.pathname;
-
-//   if (pathname === '/') {
-//     NextResponse.next();
-//   }
-// }
