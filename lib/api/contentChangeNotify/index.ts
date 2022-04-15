d4f068a27b91d32a654e3bd3c15536ef0124bd23
M.K. Safi <msafi@msafi.com>
2022-04-15 13:06:53 -0700

    let errors propagate

--- lib/api/contentChangeNotify/index.ts
+++ lib/api/contentChangeNotify/index.ts
@@ -10,19 +10,13 @@ export async function contentChangeNotify(
     return res.status(401).json({ message: 'Unauthorized' });
   }
 
-  try {
-    const { body } = req;
+  const { body } = req;
 
-    await res.unstable_revalidate(`/${body.slug}`);
+  await res.unstable_revalidate(`/${body.slug}`);
 
-    if (body.operation === 'create') {
-      await performPostPublishChores(body.forumLink, body.slug);
-    }
-
-    return res.json({ revalidated: true });
-  } catch (err) {
-    // If there was an error, Next.js will continue
-    // to show the last successfully generated page
-    return res.status(500).send('Error in content-change-notify');
+  if (body.operation === 'create') {
+    await performPostPublishChores(body.forumLink, body.slug);
   }
+
+  return res.json({ revalidated: true });
 }
