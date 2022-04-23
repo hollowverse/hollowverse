fc20128172f54e2f5f4d3ac0bb7ab8015f877dbd
Logan <98792682+LogansWebServices@users.noreply.github.com>
2022-04-23 14:18:25 -0400

    init

--- lib/components/CelebGallery/index.tsx
+++ lib/components/CelebGallery/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import Image from 'next/image';
 import { sanityImage } from '~/lib/components/sanityio';
-import { Typography } from '@mui/material';
-import s from './styles.module.scss';
 import { TCelebGalleryItem } from '~/lib/components/types';
 import { useCelebContext } from '~/lib/components/StaticPropsContextProvider';
+
 
 export const CelebGallery: React.FC<{
   celebGalleryItems: TCelebGalleryItem[];
@@ -12,38 +11,30 @@ export const CelebGallery: React.FC<{
   const context = useCelebContext();
 
   return (
-    <div className={s.CelebGallery}>
+    <div className="flex w-full h-screen bg-black">
       {p.celebGalleryItems.map((celebData) => {
         const picture = celebData.picture || context.placeholderImage;
 
         return (
           <a
-            className={s.link}
+            className=""
             href={`/${celebData.slug}`}
             key={celebData.slug}
           >
-            <span className={s.image}>
+            <span className="">
               <Image
-                objectFit="cover"
-                objectPosition="top"
-                blurDataURL={picture.metadata.lqip}
-                placeholder="blur"
                 src={sanityImage(picture).width(200).height(250).url()}
                 alt={celebData.name}
-                layout="fixed"
                 width={160}
                 height={200}
-                className={s.image}
+                className=""
               />
             </span>
-            <Typography
-              fontWeight={500}
-              variant="h4"
-              component="p"
-              className={s.name}
+            <div
+              className=""
             >
               {celebData.name}
-            </Typography>
+            </div>
           </a>
         );
       })}
