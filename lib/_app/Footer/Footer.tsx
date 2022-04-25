94f316d9da4b3bdc672e813c10c64172cec32147
M.K. Safi <msafi@msafi.com>
2022-04-25 11:13:02 -0700

    remove commented code

--- lib/_app/Footer/Footer.tsx
+++ lib/_app/Footer/Footer.tsx
@@ -11,7 +11,6 @@ import { Separator } from '~/lib/components/Separator';
 export const Footer = () => {
   return (
     <footer className={s.Footer}>
-      {/* <Separator /> */}
 
       <Container maxWidth="md" className={s.content}>
         <div className="pt-28 pb-10 flex flex-col items-center justify-center w-full h-auto">
@@ -30,68 +29,8 @@ export const Footer = () => {
             <Typography className="font-primary text-[#747474] text-sm mt-4 text-center" variant="h4" component="p">
               Email us at <a href="mailto:hollowverse@hollowverse.com" className="text-black font-medium">hollowverse@hollowverse.com</a>.
             </Typography>
-            <Typography className="font-primary text-[#747474] text-sm mt-4 text-center" variant="h4" component="p">
-              We reserve none. Everything here is free, <br />including <a className="text-black font-medium" href="https://github.com/hollowverse/" target="_blank" rel="noreferrer noopener">the code.</a>
-            </Typography>
           </div>
         </div>
-
-        {/* <div className={s.items}>
-          {(
-            [
-              [
-                InfoCircle,
-                <>About</>,
-                <>
-                  Hollowverse is about the important people and their beliefs.
-                </>,
-              ],
-              [
-                CommentDots,
-                <>Contact</>,
-                <>
-                  Email us at{' '}
-                  <Link
-                    rel="noopener"
-                    href="mailto:hollowverse@hollowverse.com"
-                    target="_blank"
-                    color="inherit"
-                  >
-                    hollowverse@hollowverse.com
-                  </Link>
-                  .
-                </>,
-              ],
-              [
-                GiftIcon,
-                <>Copyrights</>,
-                <>
-                  We reserve none. Everything here is free, including{' '}
-                  <Link
-                    href="https://github.com/hollowverse/"
-                    target="_blank"
-                    color="inherit"
-                    rel="noopener"
-                  >
-                    the code
-                  </Link>
-                  .
-                </>,
-              ],
-            ] as const
-          ).map(([IconComponent, title, text], i) => {
-            return (
-              <div key={i}>
-                <Typography variant="h4" component="p">
-                  <Icon component={IconComponent} style={{ fontSize: 17 }} />{' '}
-                  {title}
-                </Typography>
-
-                <Typography variant="body1">{text}</Typography>
-              </div>
-            );
-          })}
-        </div> */}
       </Container>
     </footer>
   );
