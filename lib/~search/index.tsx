e52604d371ddcb0134fbcc2859570ef596af080a
M.K. Safi <msafi@msafi.com>
2022-05-04 13:29:15 -0700

    Create kgCall

--- lib/~search/index.tsx
+++ lib/~search/index.tsx
@@ -3,9 +3,14 @@ import Head from 'next/head';
 import React, { useLayoutEffect, useState } from 'react';
 import s from './styles.module.scss';
 import { DebounceInput } from 'react-debounce-input';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import { isString } from 'lodash-es';
+import { kgCall } from '~/lib/~search/kgCall';
 
 export const Search = () => {
   const [val, setVal] = useState('');
+  const { previousUrl } = useRouter().query;
 
   const inputRef = React.useRef<HTMLInputElement>(null);
 
@@ -20,13 +25,24 @@ export const Search = () => {
       </Head>
 
       <Container className={s.Contribute} maxWidth="md">
-        <DebounceInput
-          inputRef={inputRef}
-          autoFocus
-          minLength={2}
-          debounceTimeout={600}
-          onChange={(event) => setVal(event.target.value)}
-        />
+        <div style={{ display: 'flex' }}>
+          <DebounceInput
+            value={val}
+            inputRef={inputRef}
+            autoFocus
+            minLength={2}
+            debounceTimeout={600}
+            onChange={async (event) => {
+              const results = await kgCall(event.target.value);
+
+              setVal(event.target.value);
+            }}
+          />
+
+          <button onClick={() => setVal('')}>X</button>
+
+          {isString(previousUrl) && <Link href={previousUrl}>Go back</Link>}
+        </div>
 
         <p>Value: {val}</p>
       </Container>
