db89eef23e1b59fdcee85503621cce02667d9b3b
M.K. Safi <msafi@msafi.com>
2022-05-10 11:35:26 -0700

    Rebase on main

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
+const { withSentryConfig } = require('@sentry/nextjs');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-module.exports = withBundleAnalyzer({
+const nextJsConfig = {
   reactStrictMode: true,
 
   images: {
@@ -34,4 +35,21 @@ module.exports = withBundleAnalyzer({
     locales: ['en'],
     defaultLocale: 'en',
   },
-});
+};
+
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+module.exports = withSentryConfig(
+  withBundleAnalyzer(nextJsConfig),
+  sentryWebpackPluginOptions,
+);
