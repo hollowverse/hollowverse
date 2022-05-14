fba3653109e128be3f62b642cb186450727a1c46
M.K. Safi <msafi@msafi.com>
2022-05-14 16:24:36 -0700

    Fix types issue

--- lib/pages/components/Spinner.tsx
+++ lib/pages/components/Spinner.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { CgSpinner } from 'react-icons/Cg';
+import { CgSpinner } from 'react-icons/cg';
 
 export function Spinner() {
   return <CgSpinner className="animate-spin text-6xl text-purple-300" />;
