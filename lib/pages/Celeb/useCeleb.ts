6f3468fbac81bb0584c155db0a363bee7ee462af
M.K. Safi <msafi@msafi.com>
2022-05-15 15:35:33 -0700

    Separate missing Facts and Celebs logging (#96)
    * Log missing facts

--- lib/pages/Celeb/useCeleb.ts
+++ lib/pages/Celeb/useCeleb.ts
@@ -1,12 +1,12 @@
 import { isEmpty } from 'lodash-es';
 import { useEffect } from 'react';
-import { logMissingCeleb } from '~/lib/pages/utils/logMissingCeleb';
+import { logMissingFacts } from '~/lib/pages/utils/logMissingCeleb';
 import { GroupedFacts } from '~/lib/pages/utils/types';
 
 export function useCeleb(name: string, factGroups: GroupedFacts['groups']) {
   useEffect(() => {
     if (isEmpty(factGroups)) {
-      logMissingCeleb(name);
+      logMissingFacts(name);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
--- lib/pages/utils/logMissingCeleb.ts
+++ lib/pages/utils/logMissingCeleb.ts
@@ -3,3 +3,7 @@ import { gaEvent } from '~/lib/pages/utils/gtag';
 export function logMissingCeleb(celebName: string) {
   gaEvent('missingCeleb', celebName);
 }
+
+export function logMissingFacts(celebName: string) {
+  gaEvent('missingFacts', celebName);
+}
