18bd3debb1c9bccffb19144f8c3c1ed898ffa56a
M.K. Safi <msafi@msafi.com>
2022-05-25 13:39:53 -0700

    fact comp 40%

--- components/Fact.tsx
+++ components/Fact.tsx
@@ -6,21 +6,23 @@ import { useFact } from '~/components/hooks/useFact';
 import { Tag } from '~/components/Tag';
 import { pluralize } from '~/lib/pluralize';
 import { Fact as TFact } from '~/lib/groq/fact.partial.groq';
+import { defaultTo } from 'lodash-es';
 
-export const Fact: React.FC<{ value: TFact; celebName: string }> = ({
-  value,
-  celebName,
-}) => {
-  const { ref, commentCount } = useFact(value);
+export const Fact: React.FC<{
+  value: TFact;
+  celebName: string;
+  showFooter?: boolean;
+  linkToFactPage?: boolean;
+  slug: string;
+}> = (props) => {
+  const { ref, commentCount } = useFact(props.value);
+  const linkToFactPage = defaultTo(props.linkToFactPage, true);
+  const showFooter = defaultTo(props.showFooter, true);
 
-  return (
-    <section
-      aria-label="Celebrity Fact"
-      className="flex flex-col gap-5 p-5"
-      ref={ref}
-    >
+  const factContent = (
+    <>
       <div className="flex flex-wrap items-center gap-2.5">
-        {value.tags.map((t) => {
+        {props.value.tags.map((t) => {
           return (
             <Tag key={t.tag.name}>
               <span className="flex items-center gap-1 text-neutral-500">
@@ -30,49 +32,63 @@ export const Fact: React.FC<{ value: TFact; celebName: string }> = ({
             </Tag>
           );
         })}{' '}
-        <p className="text-sm text-neutral-500">{value.date}</p>
+        <p className="text-sm text-neutral-500">{props.value.date}</p>
       </div>
+
       <div>
-        {(value.type === 'quote' && (
+        {(props.value.type === 'quote' && (
           <>
             <p className="text-base">
-              {value.context}, {celebName} said
+              {props.value.context}, {props.celebName} said
             </p>
 
             <blockquote className="my-2.5 block border-l-4 border-blue-400 bg-blue-50 p-5 text-base ">
-              {value.quote}
+              {props.value.quote}
             </blockquote>
           </>
-        )) || <p>{(value as any).content}</p>}
+        )) || <p>{(props.value as any).content}</p>}
       </div>
+    </>
+  );
 
-      <div className="mx-2 -mt-3 flex gap-2.5 text-neutral-600">
-        <Link href={value.forumLink} passHref>
-          <a className="flex select-none items-center gap-1 text-base text-neutral-500 transition focus:border-blue-300">
-            <BiMessage className="text-lg" />
-            {commentCount > 0 ? (
-              <>
-                {commentCount} {pluralize(commentCount, 'comment', 'comments')}
-              </>
-            ) : (
-              <>Leave a comment</>
-            )}
-          </a>
+  return (
+    <section className="flex flex-col gap-5 p-5" ref={ref}>
+      {(linkToFactPage && (
+        <Link href={`/${}`}>
+          <a href="">{factContent}</a>
         </Link>
+      )) ||
+        factContent}
+      {showFooter && (
+        <div className="FACT-FOOTER mx-2 -mt-3 flex gap-2.5 text-neutral-600">
+          <Link href={props.value.forumLink} passHref>
+            <a className="flex select-none items-center gap-1 text-base text-neutral-500 transition focus:border-blue-300">
+              <BiMessage className="text-lg" />
+              {commentCount > 0 ? (
+                <>
+                  {commentCount}{' '}
+                  {pluralize(commentCount, 'comment', 'comments')}
+                </>
+              ) : (
+                <>Leave a comment</>
+              )}
+            </a>
+          </Link>
 
-        <div className="flex-1" />
+          <div className="flex-1" />
 
-        <Link href={value.source} passHref>
-          <a
-            rel="noreferrer"
-            target="_blank"
-            className="flex select-none items-center gap-1 text-xs text-neutral-500 transition focus:border-blue-300"
-          >
-            <BiLink className="text-base" />
-            {getSourceHost(value.source)}
-          </a>
-        </Link>
-      </div>
+          <Link href={props.value.source} passHref>
+            <a
+              rel="noreferrer"
+              target="_blank"
+              className="flex select-none items-center gap-1 text-xs text-neutral-500 transition focus:border-blue-300"
+            >
+              <BiLink className="text-base" />
+              {getSourceHost(props.value.source)}
+            </a>
+          </Link>
+        </div>
+      )}
     </section>
   );
 };
