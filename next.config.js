4dcdbd40855a3998d30fd88c1d634593ef008a15
M.K. Safi <msafi@msafi.com>
2022-06-02 18:43:02 -0700

    no message

--- next.config.js
+++ next.config.js
@@ -20,24 +20,8 @@ const nextJsConfig = {
   },
 
   i18n: {
-    locales: ['default'],
-    defaultLocale: 'default',
     localeDetection: false,
   },
 };
 
-const sentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
-
-  silent: true, // Suppresses all logs
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
-};
-
-// module.exports = withBundleAnalyzer(nextJsConfig);
-
-module.exports = withSentryConfig(nextJsConfig, sentryWebpackPluginOptions);
+module.exports = nextJsConfig;
