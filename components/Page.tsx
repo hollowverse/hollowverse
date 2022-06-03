40cb51eda32ce97969b569a3e9ce7f555c8d50b2
M.K. Safi <msafi@msafi.com>
2022-06-02 19:29:30 -0700

    no message

--- components/Page.tsx
+++ components/Page.tsx
@@ -1,5 +1,4 @@
 import React, { ReactElement, ReactNode } from 'react';
-import { c } from '~/lib/c';
 import { AppBar } from '~/components/AppBar';
 import { Footer } from '~/components/Footer';
 import Head from 'next/head';
@@ -21,48 +20,10 @@ export function Page(params: {
     <>
       <Head>
         <title>{params.title} | Hollowverse</title>
-
-        {!params.allowSearchEngines && (
-          <meta key="head-robots" name="robots" content="noindex" />
-        )}
-
-        <meta
-          key="head-viewport"
-          name="viewport"
-          content="initial-scale=1, width=device-width"
-        />
-        <meta key="head-charset" charSet="utf-8" />
-
-        <meta
-          key="head-description"
-          name="description"
-          content={params.description}
-        />
-
-        <link
-          key="head-canonical"
-          rel="canonical"
-          href={`https://hollowverse.com/${params.pathname}`}
-        />
-
-        <link
-          key="head-favicon"
-          rel="shortcut icon"
-          href="/images/logo-no-border.svg"
-        />
       </Head>
 
-      <div
-        className={c(
-          'flex min-h-screen min-w-[320px] flex-col overflow-auto bg-gray-100',
-          params.className,
-        )}
-      >
-        {params.appBar || <AppBar />}
-
+      <div>
         <main className="flex flex-1 flex-col">{params.children}</main>
-
-        {params.footer || <Footer />}
       </div>
     </>
   );
