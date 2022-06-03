dfe0cf7e40748afa45f55c032de7ac15f95333a4
M.K. Safi <msafi@msafi.com>
2022-06-02 18:29:56 -0700

    no message

--- pages/api/test-api-error.ts
+++
@@ -1,7 +0,0 @@
-import { apiHandlerWithErrorLogging } from '~/lib/apiHandlerWithErrorLogging';
-
-const handler = async () => {
-  throw new Error('API throw error test');
-};
-
-export default apiHandlerWithErrorLogging(handler);
