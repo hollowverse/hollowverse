8fae3e8306f9e2e547e802ce4929f0a736e34fda
M.K. Safi <msafi@msafi.com>
2022-06-05 10:20:22 -0700

    Researcher LaunchPad (#163)
    * implement researcher lps
    
    * Fix title
    
    * clean up

---
+++ pages/[celeb]/lp.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Card } from '~/components/Card';
+import { Page } from '~/components/Page';
+
+export type ResearcherLaunchPadProps = {
+  celebName: string;
+  pathname: string;
+  topics: string[];
+};
+
+export default function ResearcherLaunchPad(props: ResearcherLaunchPadProps) {
+  return (
+    <Page
+      title={`Research ${props.celebName}'s religion and political views`}
+      description={`Hollowverse researcher's launch pad for researching ${props.celebName}`}
+      allowSearchEngines={false}
+      pathname={props.pathname}
+    >
+      <div className="mx-auto mt-5 flex w-full max-w-3xl flex-col gap-5">
+        <Card
+          title={`Suggested Google searches to research ${props.celebName}'s views`}
+        >
+          <div className="flex flex-wrap gap-5">
+            {props.topics.map((t) => {
+              return (
+                <a
+                  rel="noreferrer"
+                  target="_blank"
+                  className="h-link rounded-full border p-1 px-3"
+                  key={t}
+                  href={`https://www.google.com/search?q=${encodeURIComponent(
+                    `${props.celebName} ${t}`,
+                  )}`}
+                >
+                  {t}
+                </a>
+              );
+            })}
+          </div>
+        </Card>
+      </div>
+    </Page>
+  );
+}
+
+export { getStaticProps } from '~/lib/getStatic/launchPadCeleb.getStaticProps';
+export { getStaticPaths } from '~/lib/getStatic/defaultGetStaticPaths';
