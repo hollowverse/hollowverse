29a2605a6b457a54a87cda1631c0b2146daec8fc
M.K. Safi <msafi@msafi.com>
2022-06-09 11:07:54 -0700

    rename file

--- lib/nextApiClient2.ts
+++ lib/nextApiClient2.ts
@@ -1,7 +1,11 @@
+import { RequestInit } from 'next/dist/server/web/spec-extension/request';
 import { log } from '~/lib/log';
 
-export async function NextApiClient(...args: Parameters<typeof fetch>) {
-  const res = await fetch(...args);
+export async function nextApiClient(pathname: string, init?: RequestInit) {
+  const url = `/api/${pathname}`;
+  log('info', 'next api call', [url]);
+
+  const res = await fetch(url, init);
 
   if (!res.ok) {
     const contentType = res.headers.get('content-type');
@@ -9,7 +13,7 @@ export async function NextApiClient(...args: Parameters<typeof fetch>) {
       contentType && contentType.indexOf('application/json') !== -1;
 
     log('error', 'Next API call failed', [
-      `url: ${args[0]}; status code: ${res.status}; text: ${res.statusText}`,
+      `url: ${url}; status code: ${res.status}; text: ${res.statusText}`,
       JSON.stringify(isJson ? await res.json() : await res.text()),
     ]);
 
