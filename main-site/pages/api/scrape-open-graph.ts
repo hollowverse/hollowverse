96ea4cf2b2506f32fc24be92131369e00d2a2117
M.K. Safi <msafi@msafi.com>
2022-06-13 16:05:56 -0700

    ms: scrape open graph route

---
+++ main-site/pages/api/scrape-open-graph.ts
@@ -0,0 +1,30 @@
+import { isString } from 'lodash-es';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { apiHandlerWithErrorLogging } from '~/lib/apiHandlerWithErrorLogging';
+import { cors } from '~/lib/cors';
+import { log } from '~/shared/lib/log';
+import ogs from 'open-graph-scraper';
+
+async function scrapeOpenGraph(req: NextApiRequest, res: NextApiResponse) {
+  await cors(req, res);
+
+  const url = req.query.url;
+
+  log('info', 'scrape-open-graph', [url as string]);
+
+  if (!url || !isString(url)) {
+    throw new Error(
+      `URL query param required. Received ${JSON.stringify(url)}`,
+    );
+  }
+
+  const options = { url };
+
+  const data = await ogs(options);
+
+  const { result } = data;
+
+  return res.json({ result });
+}
+
+export default apiHandlerWithErrorLogging('scrape-open-graph', scrapeOpenGraph);
