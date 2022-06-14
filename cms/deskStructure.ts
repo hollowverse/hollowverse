ab78ec42f8a60795ef388af7def0427288c0d250
M.K. Safi <msafi@msafi.com>
2022-06-14 14:27:33 -0700

    add document pane to tags, topics, and celebs

--- cms/deskStructure.ts
+++ cms/deskStructure.ts
@@ -1,4 +1,59 @@
 import S from '@sanity/desk-tool/structure-builder';
+import groq from 'groq';
+import DocumentsPane from 'sanity-plugin-documents-pane';
+import { tag } from './schemas/tag';
+import { topic } from './schemas/topic';
+import { celeb } from './schemas/celeb';
+
+function getDocumentViews(query: string, param1: string, title: string) {
+  return S.document().views([
+    S.view.form(),
+    S.view
+      .component(DocumentsPane)
+      .options({
+        query,
+        params: {
+          param1,
+        },
+      })
+      .title(title),
+  ]);
+}
+
+export const getDefaultDocumentNode = ({ schemaType }) => {
+  if (schemaType === tag.name) {
+    return getDocumentViews(
+      groq`*[
+        _type == 'fact' &&
+        $param1 in *[_id == ^._id][0]{'tags': tags[]{'tag': tag->{name}}.tag.name}.tags
+      ]`,
+      'name',
+      'Facts',
+    );
+  }
+
+  if (schemaType === topic.name) {
+    return getDocumentViews(
+      groq`*[
+        _type == 'fact' &&
+        $param1 in *[_id == ^._id][0]{'topics': topics[]->{name}.name}.topics[]
+      ]`,
+      'name',
+      'Facts',
+    );
+  }
+
+  if (schemaType === celeb.name) {
+    return getDocumentViews(
+      groq`*[
+        _type == 'fact' &&
+        celeb._ref == $param1
+      ]`,
+      '_id',
+      'Facts',
+    );
+  }
+};
 
 export default () =>
   S.list()
