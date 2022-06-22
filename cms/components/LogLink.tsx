2e266ff910d8bb055279ba4ff1d56f484fe200c8
M.K. Safi <msafi@msafi.com>
2022-06-22 11:00:30 -0700

    fix loglink

--- cms/components/LogLink.tsx
+++ cms/components/LogLink.tsx
@@ -3,18 +3,15 @@ import { Button, Flex } from '@sanity/ui';
 import React from 'react';
 
 export const LogLink = React.forwardRef(function OpenGraphImage(props: any) {
-  const logLevels = '(level_string=info OR level_string=error)';
-  const highLogLevels =
-    '(level_string=info OR level_string=error OR level_string=debug)';
+  const logLevels = 'm.level:~"info|error"';
+  const highLogLevels = ''; // all;
 
   const [link, highLevelLink] = [logLevels, highLogLevels].map(
     (l) =>
-      `https://logtail.com/team/58257/tail?s=140937&q=${encodeURIComponent(
-        [
-          l,
-          'AND',
-          `m.requestName="${props.parent._id};${props.parent._rev}"`,
-        ].join(' '),
+      `https://logflare.app/sources/20962/search?querystring=${encodeURIComponent(
+        [l, `m.requestName:"${props.parent._id};${props.parent._rev}"`]
+          .join(' ')
+          .trim(),
       )}`,
   );
 
