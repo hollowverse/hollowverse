e4da9dcc753635d8ea2d41eddd58716650bc42c3
M.K. Safi <msafi@msafi.com>
2022-06-26 17:13:53 -0700

    rename celeb e2e

--- main-site/e2e-tests/knowledgeGraphPage.e2e.ts
+++ main-site/e2e-tests/knowledgeGraphPage.e2e.ts
@@ -2,7 +2,7 @@ import { test } from '@playwright/test';
 import { commonElementsTestFragment } from '~/e2e-tests/commonElements.e2e-test-fragment';
 import { testUrl } from '~/e2e-tests/testUrl';
 
-test.only('Knowledge Graph page E2E test', async ({ page }) => {
+test('Knowledge Graph page E2E test', async ({ page }) => {
   await page.goto(`${testUrl}/~kg/kg%3A%2Fm%2F012hshtc`);
   await page.waitForSelector('#knowledge-graph-page');
 
