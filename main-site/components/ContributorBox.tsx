554fcdbe470af6b9f691b85be6cac332beeeea44
M.K. Safi <msafi@msafi.com>
2022-07-03 16:28:25 -0700

    fix contributorbox styles

--- main-site/components/ContributorBox.tsx
+++ main-site/components/ContributorBox.tsx
@@ -13,57 +13,49 @@ export function ContributorBox(props: Contributor & { showBio?: boolean }) {
 
   return (
     <a href={getUrl(props)} target="_blank" rel="noreferrer">
-      <div id="contributor-box" className="grid auto-rows-auto">
-        <div
-          id="contributor-pic-name-container"
-          className="row-span-1 grid grid-cols-12"
-        >
-          <div id="contributor-pic" className="col-span-2 flex items-center">
-            <Image
-              className="rounded-full"
-              width={40}
-              height={40}
-              alt={`${props.username}'s image`}
-              src={props.avatar}
-            />
-          </div>
-
-          <div
-            id="contributor-name"
-            className="col-span-10 flex items-center gap-1 text-sm"
-          >
-            {props.name && (
-              <p className="truncate font-semibold">{props.name}</p>
-            )}
-            <span className="truncate font-semibold text-neutral-400">
-              @{props.username}
-            </span>
-          </div>
+      <div
+        id="contributor-box"
+        className="grid grid-cols-[min-content_auto] grid-rows-[auto_auto] gap-x-2"
+      >
+        <div id="contributor-pic" className="flex w-[40px] items-center">
+          <Image
+            className="rounded-md"
+            width={40}
+            height={40}
+            alt={`${props.username}'s image`}
+            src={props.avatar}
+          />
         </div>
 
-        <div className="grid grid-cols-12">
-          <div className="col-span-2" />
-          <div className="col-span-10 flex flex-col gap-2">
-            {props.bio && showBio ? (
-              <p className="pointer-events-none text-sm text-neutral-600">
-                {props.bio}
-              </p>
-            ) : null}
+        <div id="contributor-name" className="flex items-center gap-1 text-sm">
+          {props.name && <p className="truncate font-semibold">{props.name}</p>}
+          <p className="truncate font-semibold text-neutral-400">
+            @{props.username}
+          </p>
+        </div>
 
-            {props.website ? (
-              <div className="flex items-center gap-1 text-neutral-500">
-                <WebsiteIcon socialNetworkName={props.socialNetworkName} />
-                <a
-                  target="_blank"
-                  rel="noreferrer"
-                  href={props.website}
-                  className="text-sm underline"
-                >
-                  {props.websiteName}
-                </a>
-              </div>
-            ) : null}
-          </div>
+        <div />
+
+        <div className="flex flex-col gap-2">
+          {props.bio && showBio ? (
+            <p className="pointer-events-none text-sm text-neutral-600">
+              {props.bio}
+            </p>
+          ) : null}
+
+          {props.website ? (
+            <div className="flex items-center gap-1 text-neutral-500">
+              <WebsiteIcon socialNetworkName={props.socialNetworkName} />
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href={props.website}
+                className="text-sm underline"
+              >
+                {props.websiteName}
+              </a>
+            </div>
+          ) : null}
         </div>
       </div>
     </a>
