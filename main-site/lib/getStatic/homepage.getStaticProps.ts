19e672454fe8a92ff4ff4cf02940c2dc5a6f50fa
M.K. Safi <msafi@msafi.com>
2022-07-05 13:03:12 -0700

    cleanup homepage.getstaticprops

--- main-site/lib/getStatic/homepage.getStaticProps.ts
+++ main-site/lib/getStatic/homepage.getStaticProps.ts
@@ -6,8 +6,8 @@ import {
   TrendingCelebs,
 } from '~/lib/getStatic/getTrendingCelebs';
 import { transformFact } from '~/lib/getStatic/transformFact';
+import { Celeb, celebProjection } from '~/lib/groq/celeb.projection';
 import { Fact, factProjection } from '~/lib/groq/fact.projection';
-import { Picture } from '~/lib/groq/picture.projection';
 import { log } from '~/shared/lib/log';
 import { sanityClient } from '~/shared/lib/sanityio';
 
@@ -21,27 +21,17 @@ export async function getStaticProps(): Promise<
 > {
   log('info', 'homepage getStaticProps called');
 
-  const trendingCelebs = await getTrendingCelebs();
-
-  const latestFacts = await sanityClient.fetch<
-    (Fact & { celeb: { name: string; Picture: Picture; slug: string } })[]
-  >(
-    'latest-page-facts',
-    groq`*[_type == 'fact'] | order(date desc)[0..49] {
-      'celeb': celeb->{
-        name,
-        'picture': picture.asset->{
-          _id,
-          'metadata': {
-            'lqip': metadata.lqip,
-            'palette': metadata.palette
-          }
-        },
-        'slug': slug.current
-      },
+  const [trendingCelebs, trendingIssues, latestFacts] = await Promise.all([
+    getTrendingCelebs(),
+    () => null,
+    sanityClient.fetch<(Fact & { celeb: Celeb })[]>(
+      'latest-page-facts',
+      groq`*[_type == 'fact'] | order(date desc)[0..49] {
+      'celeb': celeb->{${celebProjection}},
       ${factProjection}
     }`,
-  );
+    ),
+  ]);
 
   return {
     props: {
