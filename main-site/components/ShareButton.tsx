a41884169a3fe4b255813dac5e17555936789304
M.K. Safi <msafi@msafi.com>
2022-07-10 12:16:53 -0700

    share fix

--- main-site/components/ShareButton.tsx
+++ main-site/components/ShareButton.tsx
@@ -6,41 +6,40 @@ export function ShareButton(props: {
   share?: { text: string; url: string };
   className?: string;
 }) {
-  const show = true;
-
-  return (
-    (show && (
-      <button
-        className={c('text-blue-500', props.className)}
-        onClick={async () => {
-          if (show) {
-            try {
-              await global.navigator.share(
-                props.share || {
-                  text: document.title,
-                  url: document.location.href,
-                },
-              );
-            } catch (e) {}
-          }
-        }}
-      >
-        <div className="flex items-center justify-center gap-1 border-b border-blue-500">
-          {props.buttonText || 'Share this page'}
-          <MdOutlineIosShare className="text-lg" />
-        </div>
-      </button>
-    )) ||
-    null
-  );
+  return show() ? (
+    <button
+      className={c('text-blue-500', props.className)}
+      onClick={async () => {
+        if (show()) {
+          try {
+            await global.navigator.share(
+              props.share || {
+                text: document.title,
+                url: document.location.href,
+              },
+            );
+          } catch (e) {}
+        }
+      }}
+    >
+      <div className="flex items-center justify-center gap-1 border-b border-blue-500">
+        {props.buttonText || 'Share this page'}
+        <MdOutlineIosShare className="text-lg" />
+      </div>
+    </button>
+  ) : null;
 }
 
 export function InBetweenContentShareButton(
   props: Parameters<typeof ShareButton>[0],
 ) {
-  return (
+  return show() ? (
     <div className="self-center">
       <ShareButton {...props} />
     </div>
-  );
+  ) : null;
+}
+
+function show() {
+  return typeof global?.navigator?.share !== 'undefined';
 }
