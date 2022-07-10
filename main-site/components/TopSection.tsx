e26db3da384902dc3b3b75bca4f0bb7b9472fcea
M.K. Safi <msafi@msafi.com>
2022-07-10 11:54:39 -0700

    clean up topsection component

--- main-site/components/TopSection.tsx
+++ main-site/components/TopSection.tsx
@@ -1,4 +1,6 @@
 import { isEmpty } from 'lodash-es';
+import { useRouter } from 'next/router';
+import { ReactNode } from 'react';
 import { CelebImage } from '~/components/CelebImage';
 import { TagCollection } from '~/components/TagCollection';
 import { CelebWithTimeline } from '~/lib/getStatic/helpers/getCelebWithTimeline';
@@ -6,34 +8,35 @@ import { Link } from '~/lib/Link';
 
 export const TopSection = (props: CelebWithTimeline) => {
   const picture = props.picture;
+  const router = useRouter();
 
   return (
     <div className="TOP-SECTION h-container p-5">
-      <Link href={`/${props.slug}`} passHref>
-        <a>
-          <div className="flex flex-wrap items-end gap-x-5">
-            <div className="w-[150px]">
-              <CelebImage
-                className="rounded-md object-cover"
-                key={props.name + '-topSection-image'}
-                picture={picture}
-                name={props.name}
-              />
-            </div>
-            <h1 className="mt-5">
-              <span className="text-lg font-normal tracking-wide text-neutral-500">
-                The Views of
-              </span>{' '}
-              <span
-                id="main-name"
-                className="mt-1 block text-4xl font-extrabold tracking-tight text-neutral-600"
-              >
-                {props.name}
-              </span>
-            </h1>
-          </div>
-        </a>
-      </Link>
+      <div className="flex flex-wrap items-end gap-x-5">
+        <div className="w-[150px]">
+          {withLink(
+            <CelebImage
+              className="rounded-md object-cover"
+              key={props.name + '-topSection-image'}
+              picture={picture}
+              name={props.name}
+            />,
+          )}
+        </div>
+        {withLink(
+          <h1 className="mt-5">
+            <span className="text-lg font-normal tracking-wide text-neutral-500">
+              The Views of
+            </span>{' '}
+            <span
+              id="main-name"
+              className="mt-1 block text-4xl font-extrabold tracking-tight text-neutral-600"
+            >
+              {props.name}
+            </span>
+          </h1>,
+        )}
+      </div>
 
       {!isEmpty(props.tagTimeline) && (
         <div className="pt-5">
@@ -42,4 +45,18 @@ export const TopSection = (props: CelebWithTimeline) => {
       )}
     </div>
   );
+
+  function withLink(node: ReactNode) {
+    const link = `/${props.slug}`;
+
+    if (link == router.asPath) {
+      return node;
+    }
+
+    return (
+      <Link href={`/${props.slug}`} passHref>
+        <a>{node}</a>
+      </Link>
+    );
+  }
 };
