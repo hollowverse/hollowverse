1c5650ff928bb568a867490ff73444a02d1b0ea9
M.K. Safi <msafi@msafi.com>
2022-07-12 12:58:05 -0700

    optimize comment count loading

--- main-site/components/FacebookCommentsCount.tsx
+++ main-site/components/FacebookCommentsCount.tsx
@@ -1,8 +1,13 @@
 import { useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
 import { getFactPagePathname } from '~/lib/getFactPagePathname';
 import { Fact } from '~/lib/groq/fact.projection';
 
 export function FacebookCommentsCount(props: { slug: string; fact: Fact }) {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+  });
+
   useEffect(() => {
     if (window.FB) {
       window.FB?.XFBML?.parse();
@@ -10,12 +15,16 @@ export function FacebookCommentsCount(props: { slug: string; fact: Fact }) {
   });
 
   return (
-    <div
-      className="fb-comments-count"
-      data-href={`https://hollowverse.com/${getFactPagePathname(
-        props.slug,
-        props.fact,
-      )}`}
-    />
+    <div ref={ref}>
+      {inView && (
+        <div
+          className="fb-comments-count"
+          data-href={`https://hollowverse.com/${getFactPagePathname(
+            props.slug,
+            props.fact,
+          )}`}
+        />
+      )}
+    </div>
   );
 }
