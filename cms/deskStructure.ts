2d26224387799f74b84ef2401b40a74438ec0da0
M.K. Safi <msafi@msafi.com>
2022-07-12 21:39:01 -0700

    remove unused func

--- cms/deskStructure.ts
+++ cms/deskStructure.ts
@@ -5,21 +5,6 @@ import { tag } from './schemas/tag';
 import { issue } from './schemas/issue';
 import { celeb } from './schemas/celeb';
 
-function getDocumentViews(query: string, param1: string, title: string) {
-  return S.document().views([
-    S.view.form(),
-    S.view
-      .component(DocumentsPane)
-      .options({
-        query,
-        params: {
-          param1,
-        },
-      })
-      .title(title),
-  ]);
-}
-
 export const getDefaultDocumentNode = ({ schemaType }) => {
   if (schemaType === tag.name) {
     return S.document().views([
