cbd7ae3f4a00a79b393409a4c2bbd263d24f6c6a
M.K. Safi <msafi@msafi.com>
2022-09-12 18:16:37 -0700

    hook up react forms

--- main-site/lib/EditSummary.tsx
+++ main-site/lib/EditSummary.tsx
@@ -1,27 +1,81 @@
-import { TextField } from '@mui/material';
+import { Alert, TextField } from '@mui/material';
 import { CelebPageMainProps } from '~/lib/celebPageMain.getStaticProps';
+import LoadingButton from '@mui/lab/LoadingButton';
+import SaveIcon from '@mui/icons-material/Save';
+import { useForm } from 'react-hook-form';
+import { isEmpty, values } from 'lodash-es';
+import { editSummaryValidate } from '~/lib/editSummaryValidate';
+
+type EditSummaryFields = {
+  religionSummary: string;
+  polvisSummary: string;
+};
 
 export default function EditSummary(props: CelebPageMainProps) {
+  const form = useForm<EditSummaryFields>({
+    criteriaMode: 'all',
+    reValidateMode: 'onBlur',
+    resolver: (formValues) => {
+      return {
+        values: formValues,
+        errors: editSummaryValidate(formValues),
+      };
+    },
+  });
+
   return (
     <div>
-      <div className="flex flex-col gap-7">
-        {/* <h3 className="font-semibold">Religion</h3> */}
+      <form
+        className="flex flex-col gap-7"
+        onSubmit={form.handleSubmit(async (data) => {
+          console.log(data);
+        })}
+      >
         <TextField
+          {...form.register('religionSummary')}
           multiline
-          value={props.positions[0].summary}
+          defaultValue={props.positions[0].summary}
           helperText={`Write a sentence or two summarizing ${props.celeb.name}'s religious views.`}
           label={`${props.celeb.name}'s religion summary`}
+          disabled={false}
           variant="outlined"
         />
 
         <TextField
+          {...form.register('polvisSummary')}
           multiline
-          value={props.positions[1].summary}
+          defaultValue={props.positions[1].summary}
           helperText={`Write a sentence or two summarizing ${props.celeb.name}'s political views.`}
           label={`${props.celeb.name}'s political views summary`}
+          disabled={false}
           variant="outlined"
         />
-      </div>
+
+        {!isEmpty(form.formState.errors) && (
+          <div>
+            <Alert severity="error">
+              <div className="flex flex-col gap-2 text-neutral-600">
+                {values(form.formState.errors).map((v, i) => (
+                  // @ts-ignore
+                  <p key={i}>{v}</p>
+                ))}
+              </div>
+            </Alert>
+          </div>
+        )}
+
+        <LoadingButton
+          loading={false}
+          type="submit"
+          color="secondary"
+          loadingPosition="start"
+          startIcon={<SaveIcon />}
+          variant="contained"
+          className="w-fit bg-blue-500"
+        >
+          Submit Changes
+        </LoadingButton>
+      </form>
     </div>
   );
 }
---
+++ main-site/lib/editSummaryValidate.ts
@@ -0,0 +1,57 @@
+import { isValid } from 'date-fns';
+import { parseDate } from '~/lib/date';
+
+function validateDate(date: string) {
+  const parsedDate = parseDate(date);
+
+  return isValid(parsedDate);
+}
+
+export type EditFormFields = {
+  religionSummary: string | null;
+  polvisSummary: string;
+};
+
+export const editFormFieldDefinitions = {
+  religionSummary: {
+    validate: (vals: EditFormFields) => {
+      if (!vals.religionSummary) {
+        return 'Religion summary is required';
+      }
+
+      if (vals.religionSummary.length < 10) {
+        return 'Religion summary is too short.';
+      }
+
+      return null;
+    },
+  },
+
+  polvisSummary: {
+    validate: (vals: EditFormFields) => {
+      if (!vals.polvisSummary) {
+        return 'Political Views summary is required';
+      }
+
+      if (vals.polvisSummary.length < 10) {
+        return 'Political Views summary is too short.';
+      }
+
+      return null;
+    },
+  },
+} as const;
+
+export function editSummaryValidate(vals: EditFormFields) {
+  let errors: { [fieldName: string]: string } = {};
+
+  (Object.keys(vals) as Array<keyof EditFormFields>).forEach((k) => {
+    const validationResults = editFormFieldDefinitions[k].validate(vals);
+
+    if (validationResults) {
+      errors[k] = validationResults;
+    }
+  });
+
+  return errors;
+}
