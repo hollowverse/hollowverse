00d1c9999aec7f37d680bf55db3ff79e4a8d4336
M.K. Safi <msafi@msafi.com>
2022-09-14 10:42:38 -0700

    move info box to inside article

--- main-site/lib/Article.tsx
+++ main-site/lib/Article.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
+import { Alert, Avatar } from '@mui/material';
 import React, { useState } from 'react';
-import { FaRegEdit } from 'react-icons/fa';
+import { FaInfoCircle, FaRegEdit } from 'react-icons/fa';
 import { c } from '~/lib/c';
 import { CelebPageMainProps } from '~/lib/celebPageMain.getStaticProps';
 import { TitledCard } from '~/lib/TitledCard.ui';
@@ -40,7 +41,7 @@ export const Article = (props: CelebPageMainProps) => {
           ),
         }}
       >
-        <div className={s.Article}>
+        <div className={c(s.Article, 'pb-2')}>
           <div
             id="editorial"
             className={c('break-normal p-5 pb-0 leading-relaxed', {
@@ -55,6 +56,56 @@ export const Article = (props: CelebPageMainProps) => {
           >
             ({showFootnotes ? 'Hide footnotes' : 'Show footnotes'})
           </button>
+
+          <div className="px-1">
+            <Alert severity="info" className="m-2">
+              <div className="flex flex-col gap-4">
+                <div className="flex flex-col gap-2">
+                  <p>
+                    This wiki was last updated on{' '}
+                    <span className="underline">{props.wiki.date}</span> by:
+                  </p>
+
+                  <div className="flex items-center gap-3">
+                    <Avatar
+                      alt={props.wiki.name}
+                      src={`https://forum.hollowverse.com/${props.wiki.avatar.replace(
+                        '{size}',
+                        '90',
+                      )}`}
+                    />
+                    <p className="text-md font-bold">{props.wiki.name}</p>
+                  </div>
+                </div>
+
+                <div className="flex flex-col gap-3">
+                  <p>Today we're opening contributions to all readers.</p>
+
+                  <p>
+                    <span className="underline">We invite you</span> and ask you
+                    to help us update this wiki by editing it.
+                  </p>
+
+                  <div className="mt-3 flex flex-wrap items-center gap-3">
+                    <a
+                      href={props.celeb.wiki!}
+                      className="flex items-center gap-2 rounded-md bg-blue-500 py-2 px-5 text-white shadow-md"
+                    >
+                      <FaRegEdit /> Edit this wiki
+                    </a>
+
+                    <a
+                      href="https://forum.hollowverse.com/t/how-to-edit-or-create-a-wiki/7333"
+                      className="flex items-center gap-2 rounded-md border border-blue-400 px-5 py-2 shadow-sm"
+                    >
+                      <FaInfoCircle />
+                      How to edit this wiki
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </Alert>
+          </div>
         </div>
       </TitledCard>
     </article>
