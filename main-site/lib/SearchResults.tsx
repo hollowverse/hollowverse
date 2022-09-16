e2a0d279a9acb4c83712b1a4c2561cffd7b1319f
M.K. Safi <msafi@msafi.com>
2022-09-16 12:55:25 -0700

    change 'request' to 'add'

--- main-site/lib/SearchResults.tsx
+++ main-site/lib/SearchResults.tsx
@@ -44,7 +44,7 @@ export function SearchResults(params: NonNullable<SearchResults>) {
               {!result.slug && (
                 <div className="pointer-events-none absolute top-5 bottom-5 right-0 my-auto mr-[3%] flex h-[99%] flex-col justify-center bg-gradient-to-r from-transparent via-white to-white pl-24">
                   <div className="rounded-full border border-gray-300 bg-white px-5 py-1 text-xs text-gray-500 shadow-sm transition hover:border-gray-400 hover:text-gray-600 active:border-gray-500 active:text-gray-700 xs:text-sm">
-                    Request
+                    Add
                   </div>
                 </div>
               )}
